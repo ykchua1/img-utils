40e904f6922ba534c3bae8bca53a2a74769f3b24
ykchua1 <ykchua1@gmail.com>
2020-09-13 02:14:48 +0000

    Added gridline plotting to plot-loss.py

--- plot-loss.py
+++ plot-loss.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+from matplotlib.ticker import AutoMinorLocator
 import pandas as pd
 import argparse
 import numpy as np
@@ -7,14 +8,20 @@ parser = argparse.ArgumentParser()
 parser.add_argument("fps", type=str, nargs="+")
 args = parser.parse_args()
 
+fig, ax = plt.subplots()
+
 for i, fp in enumerate(args.fps):
     df = pd.read_csv(fp)
-    plt.plot(df.iloc[:, 0], (df.iloc[:, 5]/3), color='C'+str(i), label="val-"+fp[:-4])
-    plt.plot(df.iloc[:, 0], (df.iloc[:, 4]), color="C"+str(i), linestyle='dashed')
+    ax.plot(df.iloc[:, 0], (df.iloc[:, 5]/3), color='C'+str(i), label="val-"+fp[:-4])
+    ax.plot(df.iloc[:, 0], (df.iloc[:, 4]), color="C"+str(i), linestyle='dashed')
     
-plt.xlim(right=200)
-plt.ylim(top=200, bottom=0)
-plt.xlabel("epochs")
-plt.ylabel("loss")
-plt.legend()
-plt.savefig("test.png")
+minorlocator = AutoMinorLocator(5)
+ax.xaxis.set_minor_locator(minorlocator)
+ax.yaxis.set_minor_locator(minorlocator)
+ax.grid(which='both')
+ax.set_xlim(left=0, right=200)
+ax.set_ylim(top=200, bottom=0)
+ax.set_xlabel("epochs")
+ax.set_ylabel("loss")
+ax.legend()
+fig.savefig("test.png")
